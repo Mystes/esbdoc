463f67d77ab8746861697c33afa42999be51c5e8
Aleksi Mustonen <aleksi.mustonen@mystes.fi>
2015-05-25 17:21:37 +0300

    Promoed SequenceItemParameters to full class

--- ESBDoc-plugin/src/main/java/fi/mystes/esbdoc/SequenceDiagramBuilder.java
+++ ESBDoc-plugin/src/main/java/fi/mystes/esbdoc/SequenceDiagramBuilder.java
@@ -438,8 +438,6 @@ public class SequenceDiagramBuilder {
         }
     }
 
-
-
     private List<String> getLeaves(SequenceItem item){
         if (null == item) {
             return new ArrayList<String>();
@@ -447,61 +445,5 @@ public class SequenceDiagramBuilder {
         return item.getLeaves();
     }
 
-    private class SequenceItemParameters {
-        private final StringBuilder stringBuilder = new StringBuilder();
-        private final List<String> handledNodeList = new ArrayList();
-
-        private String key;
-        private String parent = null;
-
-        public SequenceItemParameters(String key){
-            this.key = key;
-        }
-
-        private StringBuilder getStringBuilder(){
-            return this.stringBuilder;
-        }
-
-        private List<String> getHandledNodeList(){
-            return this.handledNodeList;
-        }
-
-        public String getKey(){
-            return this.key;
-        }
-
-        public void setKey(String key){
-            this.key = key;
-        }
-
-        public String getParent(){
-            return this.parent;
-        }
-
-        public void setParent(String parent){
-            this.parent = parent;
-        }
-
-        public String toString(){
-            return this.getStringBuilder().toString();
-        }
-
-        public boolean containsCircularDependencies(){
-            return this.getHandledNodeList().contains(this.getKey());
-        }
-
-        public void addDependency(String from, String to){
-            this.getStringBuilder().append(dependency(from, to));
-        }
-
-        private String dependency(String from, String to){
-            return from + "->" + to + ":\n";
-        }
-
-        public void addHandledNode(String key){
-            this.getHandledNodeList().add(key);
-        }
-
-    }
 
 }
