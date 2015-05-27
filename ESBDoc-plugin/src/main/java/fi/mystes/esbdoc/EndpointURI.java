21fa640174e8fb255ce9f79962feba3e3a998016
Aleksi Mustonen <aleksi.mustonen@mystes.fi>
2015-05-27 09:23:02 +0300

    Extracted EndpointUri parsing to its own class.

---
+++ ESBDoc-plugin/src/main/java/fi/mystes/esbdoc/EndpointURI.java
@@ -0,0 +1,62 @@
+package fi.mystes.esbdoc;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created by mystes-am on 26.5.2015.
+ */
+public class EndpointURI {
+    private final String uriString;
+
+    public EndpointURI(String uriString){
+        this.uriString = uriString;
+    }
+
+    public String getTarget() {
+        //uri="http://localhost:8280/services/DeleteMacoContentsFromSSCOByQueueProxy"
+        //uri="jms:/GetMacoContentProductKeyQueueProxy?transport.jms.ConnectionFactoryJ...
+
+        if (StringUtils.startsWith(uriString, "http")) {
+            return parseHttpTarget(uriString);
+        }
+
+        if (StringUtils.startsWith(uriString, "jms")) {
+            return parseJmsTarget(uriString);
+        }
+
+        return null;
+    }
+
+    private String parseHttpTarget(String targ) {
+        try {
+            String target = null;
+            // This should be URL so  resolve  target
+            URL url = new URL(targ);
+            target = url.getFile();
+            target = target.substring(target.lastIndexOf("/") + 1, target.length());
+            //If it is  still on  notation xxx.yyy, use last part
+            if (target.lastIndexOf(".") > 0) {
+                int start = target.lastIndexOf(".") + 1;
+                int end = target.length();
+                if (target.toLowerCase().contains("endpoint")) {
+                    end = start - 1;
+                    start = 0;
+                }
+                target = target.substring(start, end);
+            }
+            return target;
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+
+    private String parseJmsTarget(String target){
+        return target.split("\\?")[0].split("/")[1];
+    }
+}
--- ESBDoc-plugin/src/main/java/fi/mystes/esbdoc/SAXHandler.java
+++ ESBDoc-plugin/src/main/java/fi/mystes/esbdoc/SAXHandler.java
@@ -5,15 +5,12 @@ import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
 import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Created by mystes-am on 26.5.2015.
+ * TODO why does this have to suck so much?
  */
-class SAXHandler extends DefaultHandler {
+public class SAXHandler extends DefaultHandler {
 
     private String root;
     private SequenceDiagramBuilder diagramBuilder;
@@ -23,7 +20,6 @@ class SAXHandler extends DefaultHandler {
 
     public SAXHandler(SequenceDiagramBuilder builder) {
         this.diagramBuilder = builder;
-
     }
 
     @Override
@@ -54,7 +50,7 @@ class SAXHandler extends DefaultHandler {
         } else if (qName.equals("address")) {
             String uriTarget = attributes.getValue("uri");
             if (uriTarget.toLowerCase().contains("proxy")) {
-                String proxy = getTargetFromEndPointURI(attributes.getValue("uri"));
+                String proxy = new EndpointURI(attributes.getValue("uri")).getTarget();
                 if (proxy != null) {
                     callProxyOnDemand(proxy);
                 } else {
@@ -85,7 +81,7 @@ class SAXHandler extends DefaultHandler {
             if (service != null) {
                 if (service.startsWith("http")) {
                     // This should be URL so  resolve  target
-                    service = getUrlTargetNode(service);
+                    service = new EndpointURI(service).getTarget();
                 }
                 callProxyOnDemand(service);
             } else {
@@ -170,30 +166,6 @@ class SAXHandler extends DefaultHandler {
         }
     }
 
-    private String getUrlTargetNode(String targ) {
-        try {
-            String target = null;
-            // This should be URL so  resolve  target
-            URL url = new URL(targ);
-            target = url.getFile();
-            target = target.substring(target.lastIndexOf("/") + 1, target.length());
-            //If it is  still on  notation xxx.yyy, use last part
-            if (target.lastIndexOf(".") > 0) {
-                int start = target.lastIndexOf(".") + 1;
-                int end = target.length();
-                if (target.toLowerCase().contains("endpoint")) {
-                    end = start - 1;
-                    start = 0;
-                }
-                target = target.substring(start, end);
-            }
-            return target;
-        } catch (MalformedURLException ex) {
-            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
-            return null;
-        }
-    }
-
     private void callSequenceOnDemand(String name) throws SAXException {
         callOnDemand(name, "sequences");
     }
@@ -227,20 +199,5 @@ class SAXHandler extends DefaultHandler {
     private boolean fileExists(String name, String dir) {
         File file = new File(this.diagramBuilder.filesHome + "/" + dir + "/" + name);
         return file.exists() && file.isFile();
-    }
-
-    private String getTargetFromEndPointURI(String uri) {
-        //uri="http://localhost:8280/services/DeleteMacoContentsFromSSCOByQueueProxy"
-        //uri="jms:/GetMacoContentProductKeyQueueProxy?transport.jms.ConnectionFactoryJ...
-
-        if (uri.startsWith("http")) {
-            return getUrlTargetNode(uri);
-        }
-
-        if (uri.startsWith("jms")) {
-            return uri.split("\\?")[0].split("/")[1];
-        }
-
-        return null;
     }
 }
