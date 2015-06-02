66a68fd8981b49f234357b7798ed5762038238c2
Aleksi Mustonen <aleksi.mustonen@mystes.fi>
2015-06-02 15:23:07 +0300

    ArtifactMap knows how to add Artifacts.

--- ESBDoc-plugin/src/main/java/fi/mystes/esbdoc/ArtifactMap.java
+++ ESBDoc-plugin/src/main/java/fi/mystes/esbdoc/ArtifactMap.java
@@ -1,5 +1,7 @@
 package fi.mystes.esbdoc;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.Collection;
 import java.util.TreeMap;
 
@@ -9,5 +11,16 @@ import java.util.TreeMap;
 public class ArtifactMap extends TreeMap<String, Artifact> {
     public Collection<Artifact> getArtifacts(){
         return this.values();
+    }
+
+    public boolean addValid(Artifact artifact){
+        if(null == artifact){
+            return false;
+        }
+        if(StringUtils.isBlank(artifact.getName())){
+            return false;
+        }
+        this.put(artifact.getName(), artifact);
+        return true;
     }
 }
