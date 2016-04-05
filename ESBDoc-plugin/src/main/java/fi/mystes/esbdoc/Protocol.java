5eeed70d4301827135139c3603a8d9592e78ccd5
Aleksi Mustonen <aleksi.mustonen@mystes.fi>
2016-04-05 11:06:42 +0300

    EsbDoc should not fail if an URI cannot be parsed. It should just be ignored.

--- ESBDoc-plugin/src/main/java/fi/mystes/esbdoc/Protocol.java
+++ ESBDoc-plugin/src/main/java/fi/mystes/esbdoc/Protocol.java
@@ -1,5 +1,6 @@
 package fi.mystes.esbdoc;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.EnumUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
@@ -14,25 +15,31 @@ public class Protocol {
     private static Log log = LogFactory.getLog(Protocol.class);
 
     public enum Scheme {
-        HTTP, HTTPS, MAILTO, VFS, JMS, GOV, CONF
+        HTTP, HTTPS, MAILTO, VFS, JMS, GOV, CONF, NULL
     }
 
     public static Scheme scheme(URI uri) throws EsbDocException {
         if(null == uri){
             log.warn("Null URI submitted. Cannot extract URI Scheme.");
-            return null;
+            return Scheme.NULL;
         }
 
         String schemeString = uri.getScheme();
         if(StringUtils.isBlank(schemeString)){
-            throw new EsbDocException("Blank URI scheme for URI: " + uri.toString());
+            log.warn("Blank URI scheme for URI: " + uri.toString());
+            return Scheme.NULL;
         }
+
+        schemeString = schemeString.toUpperCase();
 
         if(!EnumUtils.isValidEnum(Scheme.class, schemeString)){
-            log.warn("Unrecognized URI scheme for URI: " + uri.toString());
-            return null;
+            StringBuilder sb = new StringBuilder();
+            sb.append("Unrecognized URI scheme for URI: " + uri.toString());
+            sb.append(". Unrecognized URI scheme was: " + schemeString);
+            sb.append(". Supported schemes are: " + ArrayUtils.toString(Scheme.values()));
+            throw new EsbDocException(sb.toString());
         }
 
-        return Scheme.valueOf(schemeString.toUpperCase());
+        return Scheme.valueOf(schemeString);
     }
 }
