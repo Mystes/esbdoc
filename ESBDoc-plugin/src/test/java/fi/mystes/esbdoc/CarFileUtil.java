afd029bcd8494a0884ac40aaa8231890668aed0a
Aleksi Mustonen <aleksi.mustonen@mystes.fi>
2016-05-31 16:18:27 +0300

    Test support logic for test folders and creation of multiple CARs in folder

--- ESBDoc-plugin/src/test/java/fi/mystes/esbdoc/CarFileUtil.java
+++ ESBDoc-plugin/src/test/java/fi/mystes/esbdoc/CarFileUtil.java
@@ -20,4 +20,24 @@ public class CarFileUtil {
 
         return new File(targetArchive);
     }
+
+    public static File createCarFile(String testName, String carName) throws Exception {
+        URL testResourcesUrl = CarAnalyzer.class.getResource("/" + testName);
+        URL carResourcesUrl = CarAnalyzer.class.getResource("/" + testName + "/" + carName);
+
+        String sourceFolder = carResourcesUrl.getPath();
+        String targetFolder = testResourcesUrl.getPath();
+
+        String targetArchive = targetFolder + "/" + carName + ".car";
+
+        Zipper.zipFolder(sourceFolder, targetArchive);
+
+        return new File(targetArchive);
+    }
+
+    public static File getTestFolder(String testName, String soapUiFolderName) {
+        URL testFolderUrl = CarAnalyzer.class.getResource("/" + testName + "/" + soapUiFolderName);
+        String testFolderPath = testFolderUrl.getPath();
+        return new File(testFolderPath);
+    }
 }
